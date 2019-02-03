4f8a1968a4b7bef0df95a546d293c9ccdd4fd1ce
sarthak-1998 <sarthak240498@gmail.com>
2019-02-03 14:19:48 +0530

    added codes

---
+++ L8d.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+
+using namespace std;
+
+bool isSafe(int mat[][10],int i , int j){
+
+	
+}
+
+void sudokuSolver( int mat[9][9],int i, int j, int n){
+
+
+}
+
+int main(){
+
+
+	return 0;
+}
